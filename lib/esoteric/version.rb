0708cb7da16f9927046332fb6ff506f02bb79636
faultier <roteshund+github@gmail.com>
2009-02-20 23:36:26 +0900

    ChangeLogとか書いてなかった

--- lib/esoteric/version.rb
+++ lib/esoteric/version.rb
@@ -5,7 +5,7 @@ module Esoteric
     unless defined? MAJOR
       MAJOR = 0
       MINOR = 0
-      TINY  = 1
+      TINY  = 2
       MINESCULE = nil
 
       STRING = [MAJOR, MINOR, TINY, MINESCULE].compact.join('.')
